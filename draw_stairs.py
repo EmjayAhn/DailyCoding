52794b6e2dc393bae338ae38b19159a89f2de884
EmjayAhn <emjay.data@gmail.com>
2018-12-20 23:32:55 +0900

    Feat: codewars draw_stairs algorithm quiz

---
+++ draw_stairs.py
@@ -0,0 +1,18 @@
+# given a number n, draw stairs with 'I' n tall and n wide,
+# with the tallest in the top left.
+# Example (with - to represent spaces;
+# DO NOT USE THEM IN THE SOLUTIONS!
+# USE SPACES IN SOLUTION!
+# the "-"s are for clarity.)
+# draw_stairs(3) == '''I\n_I\n__I'''
+
+def draw_stairs(n):
+    # do something
+    wide = ' '
+    result = ""
+    for index in range(n):
+        result += index * wide + 'I'
+        if index != n - 1:
+            result += '\n'
+
+    return result
